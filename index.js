c17796c0ac4f1472c7cb814462114d895fceae37
Albana Balla <138111535+AlbanaBalla@users.noreply.github.com>
2023-11-15 14:49:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const posts = [
     post: "images/post-vangogh.jpg",
     comment: "just took a few mushrooms lol",
     likes: 21,
+    liked: false
   },
   {
     name: "Gustave Courbet",
@@ -16,6 +17,7 @@ const posts = [
     post: "images/post-courbet.jpg",
     comment: "i'm feelin a bit stressed tbh",
     likes: 4,
+    liked: false
   },
   {
     name: "Joseph Ducreux",
@@ -26,5 +28,47 @@ const posts = [
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
     likes: 152,
+    liked: false
   },
-];
+]
+
+
+let heart = "images/icon-heart.png"
+let comment = "images/icon-comment.png"
+let dm = "images/icon-dm.png"
+let likeIcon = "images/like.png"
+
+const mainEl = document.getElementById("post")
+ let html = ""
+ for (let i = 0; i < posts.length; i++) {
+     html += `
+         <article class="post">
+                 <div class="header container">
+                     <img class="avatar" src="${posts[i].avatar}">
+                     <div>
+                         <div>
+                             <span class="name">${posts[i].name}</span>
+                         </div>
+                         <span class="location">${posts[i].location}</span>
+                     </div>
+                 </div>
+                 <div>
+                     <img class="img" src="${posts[i].post}">
+                 </div>
+                 <div class="control-bar container">
+                     <img class="heart" src="icon-heart.png">
+                     <img class="comment" src="icon-comment.png">
+                     <img class="dm" src="icon-dm.png">                    
+                 </div>
+                 <div class="container">
+                     <span class="likes">${posts[i].likes} likes</span>
+                 </div>
+                 <div class="container">
+                     <p><span class="username">${posts[i].username}</span> ${posts[i].comment}</p>
+                 </div>
+             </article>
+     `
+ }
+
+ mainEl.innerHTML = html
+ 
